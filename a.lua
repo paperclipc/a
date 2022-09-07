d622b28ee0281bb174469c3f5180e24d85859a3b
Paperclip <97054043+paperclipc@users.noreply.github.com>
2022-09-07 18:16:43 -0400

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,3 +1,11 @@
+--// Anti Afk
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
 --// Get Services
 local sPlayer = game:GetService("Players")
 local sRep = game:GetService("ReplicatedStorage")
